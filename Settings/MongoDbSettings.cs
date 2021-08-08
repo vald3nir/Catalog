f4c94b66c30cc0078d5d6ac2e6a8d664157f01b3
Valdenir Severino <vald3nir@gmail.com>
2021-08-08 09:24:27 -0300

    add mongodb setup for production

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -4,11 +4,21 @@ namespace Catalog.Settings
     {
         public string Host { get; set; }
         public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public bool InProd { get; set; }
         public string ConnectionString
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";
+                if (InProd)
+                {
+                    return $"mongodb+srv://{User}:{Password}@examples.ksqsw.mongodb.net/catalog?retryWrites=true&w=majority";
+                }
+                else
+                {
+                    return $"mongodb://{Host}:{Port}";
+                }
             }
         }
     }
