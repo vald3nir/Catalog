810b556bdfe1bc2e4262be9704e3fd2fecc870b3
Valdenir Severino <vald3nir@gmail.com>
2021-08-10 10:08:18 -0300

    deploy to docker [prod]

--- Startup.cs
+++ Startup.cs
@@ -70,10 +70,12 @@ namespace Catalog
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
